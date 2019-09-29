b4e747dfc756ade295971423ca0b8a19abc6354f
conectis360 <conectis360@gmail.com>
2019-09-28 21:02:46 -0300

    Adding some new models, trying some design/mockup references on post_detail and home.html and finaly a .gitgnore

--- CRUD/models.py
+++ CRUD/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django.urls import reverse
 
-
+class BlogTag(models.Model):
+    name = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=48)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+    
 class BlogPost(models.Model):
     title = models.CharField(max_length=60)
     text_body = models.TextField()
+    tags = models.ManyToManyField(BlogTag, blank=True)
     escritor = models.ForeignKey(
         'auth.User', 
         on_delete=models.CASCADE
@@ -12,3 +18,4 @@ class BlogPost(models.Model):
     
     def __str__(self):
         return self.title
+    
